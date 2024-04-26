25486dfb6f786c0785885b4319c500a5b26513d1
guillaumebrethes <herver86@gmail.com>
2024-04-26 22:22:20 +0200

    form d'affichage df

--- Streamlit/pages/2_📖_Presentation_des_données.py
+++ Streamlit/pages/2_📖_Presentation_des_données.py
@@ -47,4 +47,43 @@ if st.checkbox(
     label="Contenu du Dataset", 
     key=checkbox_key, 
     help="Cliquez pour afficher le contenu du dataset"):
-        st.dataframe(df.head())
+    
+    # Sélection numérique pour choisir le nombre de lignes à afficher
+    num_rows = st.number_input(
+        label="Nombre de lignes à afficher",
+        help="Maximun 50 lignes",
+        min_value= 1,  
+        max_value= 50,  
+        step= 1,  # Incrément
+        value= 5  # défaut
+    )
+
+    # Afficher les premières 'num_rows' lignes du DataFrame
+    st.dataframe(df.head(num_rows))
+    
+    # Diviser l'espace en deux colonnes
+    col1, col2 = st.columns(2)
+    
+    # Premier widget de sélection numérique pour choisir le numéro de la première ligne
+    with col1:
+        start_row = st.number_input(
+            label="Première ligne à afficher",
+            min_value = 0,  
+            max_value = len(df) - 1, 
+            step= 1, 
+            value= 0  
+        ) 
+    
+    # Deuxième widget de sélection numérique pour choisir le numéro de la dernière ligne
+    with col2:
+        end_row = st.number_input(
+            label="Dernière ligne à afficher",
+            min_value=start_row,  # Valeur minimale autorisée
+            max_value=len(df),  # Nombre maximum de lignes du DataFrame
+            step=1,  # Incrément
+            value=min(start_row + 5, len(df))  # Valeur par défaut
+        )
+
+    # Afficher les lignes sélectionnées du DataFrame
+    st.dataframe(df.iloc[start_row:end_row])
+

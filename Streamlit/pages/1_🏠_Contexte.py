7391a500173e90f5426c1e6edca51a9a9fe380da
guillaumebrethes <herver86@gmail.com>
2024-05-02 22:35:02 +0200

    Banque.jpg to banque.jpg 3

--- Streamlit/pages/1_🏠_Contexte.py
+++ Streamlit/pages/1_🏠_Contexte.py
@@ -26,7 +26,7 @@ st.write(
     "L'objectif est de prédire quels clients sont les plus susceptibles de souscrire au dépôt à terme."
     )
 
-st.image("/Users/gub/Documents/Privé/Formations/DataScientest/Data_projet/Oct_cda_bankmarketing/Streamlit/Banque.jpg", 
+st.image("Banque.jpg", 
          width=500, 
          use_column_width='always', 
          output_format='auto')

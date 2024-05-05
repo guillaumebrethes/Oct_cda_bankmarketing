acf3dac1b7a41998fa13ce32f017c8ddc6fe89e8
guillaumebrethes <herver86@gmail.com>
2024-05-05 08:17:01 +0200

    page 1/ chang path banque.jpg

--- Streamlit/pages/1_🏠_Contexte.py
+++ Streamlit/pages/1_🏠_Contexte.py
@@ -13,7 +13,7 @@ st.write(
     "Ce projet s'inscrit dans le cadre de l'utilisation des sciences des données appliquées dans les entreprises de service et plus précisément dans le domaine bancaire.\n\n Au sein du secteur bancaire,l'optimisation du ciblage du télémarketing est un enjeu clé, sous la pression croissante d'augmenter les profits et de réduire les coûts.\n\n Nous avons à disposition les données de la dernière campagne télémarketing d'une banque pour la vente de dépôts à terme. Ce jeu de données est en accès libre sur la plateforme Kaggle.com.\n\n L'objectif est de prédire quels clients sont les plus susceptibles de souscrire au dépôt à terme."
 )
 
-url_image_contexte = "/Users/gub/Documents/Privé/Formations/DataScientest/Data_projet/Oct_cda_bankmarketing/Streamlit/pages/banque.jpg"
+url_image_contexte = "Streamlit/pages/banque.jpg"
 st.image(url_image_contexte,
          width=500, 
          use_column_width='always', 

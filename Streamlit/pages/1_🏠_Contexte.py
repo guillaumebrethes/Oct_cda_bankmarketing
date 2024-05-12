9035bf7b150e6c214e5917ad581b37f4f3738887
guillaumebrethes <herver86@gmail.com>
2024-05-12 21:01:38 +0200

    changment des chemins comme estelle

--- Streamlit/pages/1_🏠_Contexte.py
+++ Streamlit/pages/1_🏠_Contexte.py
@@ -12,7 +12,7 @@ st.set_page_config(
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-local_css("Streamlit/styles.css")
+local_css("styles.css")
 # - - - - - - - 
 
 st.markdown('<h1 class="custom-title">Contexte du projet</h1>', unsafe_allow_html=True)
@@ -23,7 +23,7 @@ st.write(
     "Ce projet s'inscrit dans le cadre de l'utilisation des sciences des données appliquées dans les entreprises de service et plus précisément dans le domaine bancaire.\n\n Au sein du secteur bancaire,l'optimisation du ciblage du télémarketing est un enjeu clé, sous la pression croissante d'augmenter les profits et de réduire les coûts.\n\n Nous avons à disposition les données de la dernière campagne télémarketing d'une banque pour la vente de dépôts à terme. Ce jeu de données est en accès libre sur la plateforme Kaggle.com.\n\n L'objectif est de prédire quels clients sont les plus susceptibles de souscrire au dépôt à terme."
 )
 
-url_image_contexte = "Streamlit/pages/banque.jpg"
+url_image_contexte = "pages/banque.jpg"
 st.image(url_image_contexte,
          width=500, 
          use_column_width='always', 
--- Streamlit/pages/4_⚙️_Modelisation.py
+++ Streamlit/pages/4_⚙️_Modelisation.py
@@ -18,7 +18,7 @@ st.set_page_config(
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-local_css("Streamlit/styles.css")
+local_css("styles.css")
 # - - - - - - - 
 
 # titre

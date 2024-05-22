35c3a21a7b7f3bd44503eba3350d86b223a24a4e
guillaumebrethes <herver86@gmail.com>
2024-05-22 22:54:50 +0200

    fix1

--- Streamlit/pages/9_💬_Contacts.py
+++ Streamlit/pages/9_💬_Contacts.py
@@ -14,7 +14,14 @@ local_css("styles.css")
 
 st.markdown('<h1 class="custom-title">Contacts</h1>', unsafe_allow_html=True)
 
+
+
 if st.button("◀️\u2003 🎲 Avez vous souscrit ?"):
     st.switch_page("pages/7_🎲_Avez_vous_souscrit?.py")
 st.markdown('<hr class="my_custom_hr">', unsafe_allow_html=True)
+
+
+st.markdown("""
+            a remplir 
+            """)
 
